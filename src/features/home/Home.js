492e83d7b7345f6b0989d1c0afc153eda8a78ca5
bunbebu <nguyenhongphuc0703@gmail.com>
2025-10-05 11:53:18 +0700

    deploy

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div style={{ padding: 24 }}>
-      <h1>Welcome to the Home page (demo)</h1>
+      <h1>Welcome to the Home page</h1>
     </div>
   );
 }
